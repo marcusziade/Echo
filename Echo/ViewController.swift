1203d6031383d41349d08d499aa523d6b4bb1fa8
Marcus Ziadé <guitaripod@icloud.com>
2025-05-29 23:25:35 +0300

    Setup project

--- Echo/ViewController.swift
+++ Echo/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Echo
-//
-//  Created by Marcus Ziadé on 5/29/25.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
 }
-
